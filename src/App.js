3ddb3c3729cc9bf0ed47ce48dde084fb92f9fb91
Julian88V <juliangoudy1@yahoo.com>
2020-09-18 14:24:39 -0400

    set up user and auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 import CreateProperty from "./components/create-property.component";
 import EditProperty from "./components/edit-property.component";
 import BookList from "./components/book-list.component";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/layout/navbar";
+import Landing from "./components/layout/landing";
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard.component";
+
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -25,11 +59,19 @@ function App() {
             </div>
           </nav>
           <br/>
+          <Navbar />
           <Route path="/" exact component={BookList} />
           <Route path="/edit/:id" component={EditProperty} />
           <Route path="/create" component={CreateProperty} />
+          <Route exact path="/landing" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
         </div>
       </Router>
+      </Provider>
   );
 }
 
